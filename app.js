24f3528c99724c02bb382723dde53d6bf9c542cd
Grosul Igor <grosuligor99@mail.ru>
2022-12-23 17:15:59 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-"use strict" 
+"use strict"
 
 
 function calculateSplit() {
     let amountValue = +document.getElementById('attemptAmount').value;
     let tipValue = +document.getElementById('serviceTip').value;
     let attemptPeople = +document.getElementById('numberPeople').value;
-    if (!isFinite(amountValue)) {alert("Write a number as amount value")}
-    if (!isFinite(attemptPeople)) {alert("Write a number as a number of people")}
+    if (!isFinite(amountValue)) { alert("Write a number as amount value") }
+    if (!isFinite(attemptPeople)) { alert("Write a number as a number of people") }
     let newTip = amountValue * tipValue;
     let newAmount = amountValue + newTip;
     let resultAmount = newAmount / attemptPeople;
-    let resultAmount1 = Math.round(resultAmount*100) / 100;
+    let resultAmount1 = Math.round(resultAmount * 100) / 100;
     document.getElementById('resultText').innerHTML = 'TIP AMOUNT' + '<br>' + resultAmount1 + "$" + '<br>' + "for each";
 }
