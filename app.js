ea6fede931da486ff68479095e944bc0e4692470
Amathia <grosuligor99@mail.ru>
2023-03-24 23:07:33 +0200

    fixbug

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ function calculateSplit() {
     let amountValue = +document.getElementById('attemptAmount').value;
     let tipValue = +document.getElementById('serviceTip').value;
     let attemptPeople = +document.getElementById('numberPeople').value;
-    if (!isFinite(amountValue)) { alert("Write a number as amount value") }
-    if (!isFinite(attemptPeople)) { alert("Write a number as a number of people") }
+    if (!isFinite(amountValue)) { alert("Write a number as amount value"); return; }
+    if (!isFinite(attemptPeople)) { alert("Write a number as a number of people"); return; }
     let newTip = amountValue * tipValue;
     let newAmount = amountValue + newTip;
     let resultAmount = newAmount / attemptPeople;
